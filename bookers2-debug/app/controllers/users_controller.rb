3fe5d93ad02931ed2fb346f28e0c2c53e6c8fff7
tsukasatakahashi0614 <tkspp2sko@gmail.com>
2021-06-01 11:00:28 +0000

    layout調整

--- bookers2-debug/app/controllers/users_controller.rb
+++ bookers2-debug/app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
@@ -33,5 +33,6 @@ class UsersController < ApplicationController
     unless @user == current_user
       redirect_to user_path(current_user)
     end
+  end
   end
 end
