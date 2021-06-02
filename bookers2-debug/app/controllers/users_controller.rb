0ca1258ecbe011eb0366a686d52eae95ae50a8a1
tsukasatakahashi0614 <tkspp2sko@gmail.com>
2021-06-02 03:00:12 +0000

    finish

--- bookers2-debug/app/controllers/users_controller.rb
+++ bookers2-debug/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
+  before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
@@ -10,19 +11,27 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
     @user = User.find(params[:id])
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+    if @user == current_user
+      render "edit"
     else
-      render "show"
+      redirect_to user_path(current_user)
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
@@ -31,8 +40,7 @@ class UsersController < ApplicationController
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
-      redirect_to user_path(current_user)
+      redirect_to users_path(current_user)
     end
-  end
   end
 end
