44e7b3ce4f2b9a7954bb3c4044f1548ef9b4884e
tsukasatakahashi0614 <tkspp2sko@gmail.com>
2021-06-05 01:05:59 +0000

    応用課題4FINISH

--- bookers2-debug/app/models/user.rb
+++ bookers2-debug/app/models/user.rb
@@ -7,6 +7,24 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :books
   has_many :book_comments
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+  
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
+  
+  
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers" => "relationships#followers", as: "followers"
+  end
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create,:destroy]
