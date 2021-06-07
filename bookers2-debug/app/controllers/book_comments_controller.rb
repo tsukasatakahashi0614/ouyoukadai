932e5fd99b2d274c7b1d9b12e478e548b8506bc9
tsukasatakahashi0614 <tkspp2sko@gmail.com>
2021-06-07 11:35:38 +0000

    コメント機能非同期通信化

--- bookers2-debug/app/controllers/book_comments_controller.rb
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -1,23 +1,22 @@
 class BookCommentsController < ApplicationController
+	before_action :authenticate_user!
 
-  def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
-  end
+	def create
+		@book = Book.find(params[:book_id])
+		@book_comment = BookComment.new(book_comment_params)
+		@book_comment.book_id = @book.id
+		@book_comment.user_id = current_user.id
+		@book_comment.save
+	end
 
-   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_back(fallback_location: root_path)
-   end
+	def destroy
+		@book = Book.find(params[:book_id])
+  	book_comment = @book.book_comments.find(params[:id])
+		book_comment.destroy
+	end
 
-   private
-
-   def book_comment_params
-     params.require(:book_comment).permit(:comment)
-   end
-
+	private
+	def book_comment_params
+		params.require(:book_comment).permit(:comment)
+	end
 end
